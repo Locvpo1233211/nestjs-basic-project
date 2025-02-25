1dea33ca391cec76ae0ae443f1d55b770bf29fad
Locvpo1233211 <106452148+Locvpo1233211@users.noreply.github.com>
2025-02-25 08:36:44 +0700

    Version APIs.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/passport/jwt-auth.guard';
 import { TransformInterceptor } from './auth/core/transform.interceptor';
 
@@ -15,9 +15,15 @@ async function bootstrap() {
   app.useGlobalGuards(new JwtAuthGuard(reflector));
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  console.log(join(__dirname));
   app.setViewEngine('ejs');
   app.useGlobalPipes(new ValidationPipe());
+
+  // config Version
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1', '2'],
+  });
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
