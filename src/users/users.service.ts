8eb2d9c5c6669893b0d3c06e9203d138702ab2dd
Locvpo1233211 <106452148+Locvpo1233211@users.noreply.github.com>
2025-03-09 12:58:44 +0700

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -134,9 +134,6 @@ export class UsersService {
   }
 
   async update(updateUserDto: UpdateUserDto, user: IUser) {
-    if (!mongoose.Types.ObjectId.isValid(updateUserDto._id)) {
-      throw new BadRequestException('not update user');
-    }
     return await this.userModel.updateOne(
       { _id: updateUserDto._id },
       {
